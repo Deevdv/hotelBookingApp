ef710940402ad6a5ae28de53d629560ebe75f3ea
Deevdv <110405472+Deevdv@users.noreply.github.com>
2023-08-22 00:10:01 +0200

    added pages, and finished landing page
    cards, images and links added

---
+++ pages/footer.php
@@ -0,0 +1,22 @@
+<div class="container-fluid bg-white mt-5">
+            <div class="row">
+                <div class="col-lg-4 p-4">
+                    <h3 class="h-font fw-bold fs-3 mb-2">Lux Resorts</h3>
+                    <p>At Lux Resorts we aim to revolutionize the hotel booking experience by providing a seamless, personalized, and luxurious stay for our customers through our innovative app, Lux Resorts.</p>
+                </div>
+                <div class="col-lg-4 p-4">
+                    <h5 class="mb-3">Links</h5>
+                    <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Home</a> <br>
+                    <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Hotels</a> <br>
+                    <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Facilities</a> <br>
+                    <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Contact</a> <br>
+                    <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">About</a> <br>
+                </div>
+                <div class="col-lg-4 p-4">
+                    <h5 class="mb-3">Follow us</h5>
+                    <a href="#" class="d-inline-block text-dark text-decoration-none mb-2"> <i class="bi bi-twitter me-1"></i> Twitter</a> <br>
+                    <a href="#" class="d-inline-block text-dark text-decoration-none mb-2"> <i class="bi bi-facebook me-1"></i> Facebook</a> <br>
+                    <a href="#" class="d-inline-block text-dark text-decoration-none"> <i class="bi bi-instagram me-1"></i> Instagram</a> <br>
+                </div>
+            </div>
+        </div>
---
+++ pages/links.php
@@ -0,0 +1,3 @@
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css2?family=Charm:wght@400;700&family=Raleway:wght@400;500;600&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
