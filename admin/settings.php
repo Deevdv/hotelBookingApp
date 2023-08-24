19bac611a32435051acd3c5db5a6503f2e7a2cbd
Deevdv <110405472+Deevdv@users.noreply.github.com>
2023-08-24 21:22:55 +0200

    shutdown toggle added
    update

--- admin/settings.php
+++ admin/settings.php
@@ -27,7 +27,7 @@ adminLogin();
 
                 <!-- settings -->
 
-                <div class="card">
+                <div class="card border-0 shadow-sm">
                     <div class="card-body">
                         <div class="d-flex align-items-center justify-content-between">
                             <h5 class="card-title m-0">General Settings</h5>
@@ -72,66 +72,125 @@ adminLogin();
                         </form>
                     </div>
                 </div>
-            </div>
-        </div>
-    </div>
 
-    <?php require('pages/scripts.php'); ?>
-    <script>
-        let general_data;
 
-        function get_general() {
-            let site_title = document.getElementById('site_title');
-            let site_about = document.getElementById('site_about');
+                <!--Shutdown-->
 
-            let site_title_inp = document.getElementById('site_title_inp');
-            let site_about_inp = document.getElementById('site_about_inp');
+                <div class="card border-0 shadow-sm">
+                    <div class="card-body">
+                        <div class="d-flex align-items-center justify-content-between">
+                            <h5 class="card-title m-0">ShutdownSettings</h5>
+                            <div class="form-check form-switch">
+                                <form>
+                                    <input onchange="upd_shutdown(this.value)" class="form-check-input" type="checkbox" role="switch" id="shutdown_toggle" >
+                                </form>
 
-            let xhr = new XMLHttpRequest();
-            xhr.open("POST", "ajax/settings_crud.php", true);
-            xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+                            </div>
 
-            xhr.onload = function() {
-                general_data = JSON.parse(this.responseText);
-                // console.log(general_data);
-                site_title.innerText = general_data.site_title;
-                site_about.innerText = general_data.site_about;
+                        </div>
+                        <p class="card-text">No hotel bookings will be made when shutdown mode has been activated!</p>
+                    </div>
+                </div>
 
-                site_title_inp.value = general_data.site_title;
-                site_about_inp.value = general_data.site_about;
-            }
+                <?php require('pages/scripts.php'); ?>
+                <script>
+                    let general_data;
 
-            xhr.send('get_general');
-        }
+                    function get_general() {
+                        let site_title = document.getElementById('site_title');
+                        let site_about = document.getElementById('site_about');
 
-        function upd_general(site_title_val,site_about_val) {
+                        let site_title_inp = document.getElementById('site_title_inp');
+                        let site_about_inp = document.getElementById('site_about_inp');
 
-            let xhr = new XMLHttpRequest();
-            xhr.open("POST", "ajax/settings_crud.php", true);
-            xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+                        let shutdown_toggle =document.getElementById('shutdown_toggle');
 
-            xhr.onload = function() {
+                        let xhr = new XMLHttpRequest();
+                        xhr.open("POST", "ajax/settings_crud.php", true);
+                        xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
 
-                console.log(this.responseText);
-                   // general_data = JSON.parse(this.responseText);
-                // console.log(general_data);
-               // site_title.innerText = general_data.site_title;
-                //site_about.innerText = general_data.site_about;
+                        xhr.onload = function() {
+                            general_data = JSON.parse(this.responseText);
+                            // console.log(general_data);
+                            site_title.innerText = general_data.site_title;
+                            site_about.innerText = general_data.site_about;
 
-                //site_title_inp.value = general_data.site_title;
-                //site_about_inp.value = general_data.site_about;
-            
-            }
+                            site_title_inp.value = general_data.site_title;
+                            site_about_inp.value = general_data.site_about;
 
-            xhr.send('site_title='+site_title_val+'&site_about='+site_about_val+'&upd_general');
-        }
+                            if(general_data.shutdown == 0){
+                                shutdown_toggle.checked = false;
+                                shutdown_toggle.value = 0;
+                            }
+                            else {
+                                shutdown_toggle.checked = true;
+                                shutdown_toggle.value = 1;
+                            }
+                        }
 
-        
+                        xhr.send('get_general');
+                    }
 
-        window.onload = function() {
-            get_general();
-        }
-    </script>
+                    function upd_general(site_title_val, site_about_val) {
+
+                        let xhr = new XMLHttpRequest();
+                        xhr.open("POST", "ajax/settings_crud.php", true);
+                        xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+
+                        xhr.onload = function() {
+                            var myModal = document.getElementById('general-s');
+                            var modal = bootstrap.Modal.getInstance(myModal); // Returns a Bootstrap modal instance
+                            modal.hide();
+
+                            if (this.responseText == 1) {
+                                //console.log('data updated');
+                                alert('success', 'Changes Saved!');
+                                get_general();
+                            } else {
+                                //console.log('no change');
+                                alert('failed', 'No Changes!');
+                            }
+
+                            //console.log(this.responseText);
+                            // general_data = JSON.parse(this.responseText);
+                            // console.log(general_data);
+                            // site_title.innerText = general_data.site_title;
+                            //site_about.innerText = general_data.site_about;
+
+                            //site_title_inp.value = general_data.site_title;
+                            //site_about_inp.value = general_data.site_about;
+
+                        }
+
+                        xhr.send('site_title=' + site_title_val + '&site_about=' + site_about_val + '&upd_general');
+                    }
+
+                    function upd_shutdown(val){
+                        let xhr = new XMLHttpRequest();
+                        xhr.open("POST", "ajax/settings_crud.php", true);
+                        xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+
+                        xhr.onload = function() {
+                         
+                            if (this.responseText == 1 && general_data.shutdown==0) {
+                                //console.log('data updated');
+                                alert('success', 'Shutdown on!');
+                         
+                            } else {
+                                //console.log('no change');
+                                alert('failed', 'Shutdown off!');
+                            }
+                        get_general();
+                        }
+
+                        xhr.send('upd_shutdown='+val);
+
+                    }
+
+                    window.onload = function() {
+                        get_general();
+                    }
+                </script>
 </body>
 
 </html>
